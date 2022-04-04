537e97f51a3b4bdf6b2b731174220c80a8e30f97
joksdev <67756202+joksdz@users.noreply.github.com>
2022-04-04 14:47:15 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using DSharpPlus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using DSharpPlus.EventArgs;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+
+
+namespace my_discord_bot_v2
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            MainAsync().GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync()
+        {
+            var discord = new DiscordClient(new DiscordConfiguration()
+            {
+
+                Token = "OTYwNDU2NzI5MDkzNjIzODA4.YkqtCA.1wZDEFY3IT3ODDXuv3Hs6ZlNIHU",
+                TokenType = TokenType.Bot,
+                Intents = DiscordIntents.AllUnprivileged
+            });
+
+            discord.MessageCreated += async (s, meseges) =>
+            {
+                
+                if(meseges.Message.Content.ToLower().StartsWith("ramadan karim"))
+                    await meseges.Message.RespondAsync("RamadanKom Karim    :) ");
+                if (meseges.Message.Content.ToLower().StartsWith("ey"))
+                    await meseges.Message.RespondAsync("Wsup!");
+                if (meseges.Message.Content.ToLower().StartsWith("eid mobark"))
+                    await meseges.Message.RespondAsync("EidKom Mobark  :)");
+            };
+
+
+            await discord.ConnectAsync();
+                await Task.Delay(-1);
+
+
+        }
+      
+
+
+
+
+
+}
+}
---
+++ my discord bot v2/bot.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using DSharpPlus;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace my_discord_bot_v2
+{
+    public class Bot
+    {
+
+        
+
+
+       
+
+    }
+}
